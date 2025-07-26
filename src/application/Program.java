cc958c371f26505a333261bc312bbad51a2074f3
MathDEV-0 <teteuviegas.2006@hotmail.com>
2025-07-26 18:50:36 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,20 +20,25 @@ public class Program {
 		System.out.println(seller);
 		
 		System.out.println("=== TEST 2: seller findByDepartment ===");
-		List<Seller> list = sellerDao.findByDepartment(new Department(2,"Eletronics"));
+		Department department = new Department(2,"Eletronics");
+		List<Seller> list = sellerDao.findByDepartment(department);
 		
 		for(Seller s: list) {
 			System.out.println(s);
 		}
 		
-		System.out.println("=== TEST 2: seller findAll ===");
+		System.out.println("=== TEST 3: seller findAll ===");
 		List<Seller> list2 = sellerDao.findAll();
 		
 		for(Seller s: list2) {
 			System.out.println(s);
 		}
 
+		System.out.println("=== TEST 4: seller insert ===");
 		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com",new Date(),1900.0,department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id:  " + newSeller.getId());
 		
 	}
 
